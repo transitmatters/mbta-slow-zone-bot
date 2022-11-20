904fd2565a8a4a75ee1a4d40d7928f94c34471e6
Devin Matte <devinmatte@gmail.com>
2022-11-20 14:50:51 -0800

    Adding mastodon posts alongside Twitter

--- slowzones.py
+++ slowzones.py
@@ -1,13 +1,13 @@
 import tweepy
+import mastodon as mastodon
 import requests
 from datetime import timedelta, date
+from domains.mastodon import send_fixed_slow_zone_toots, send_new_slow_zone_toots, send_toot_threads
+from domains.twitter import send_fixed_slow_zone_tweets, send_new_slow_zone_tweets, send_tweet_threads
 from utils import (
     generate_grouped_slow_zone_list,
-    generate_tweet_text_map,
-    send_tweet_threads,
+    generate_post_text_map,
     generate_new_slow_zones_list,
-    send_new_slow_zone_tweets,
-    send_fixed_slow_zone_tweets,
 )
 import os
 
@@ -16,14 +16,24 @@ ACCESS_KEY = os.environ.get("ACCESS_KEY")
 ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
 CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
-print(os.environ)
 
-client = api = tweepy.Client(
+twitter_client = api = tweepy.Client(
     bearer_token=os.environ.get("BEARER_TOKEN"),
     access_token=ACCESS_KEY,
     access_token_secret=ACCESS_SECRET,
     consumer_key=CONSUMER_KEY,
     consumer_secret=CONSUMER_SECRET,
+)
+
+MASTODON_CLIENT_KEY = os.environ.get("MASTODON_CLIENT_KEY")
+MASTODON_CLIENT_SECRET = os.environ.get("MASTODON_CLIENT_SECRET")
+MASTODON_ACCESS_TOKEN = os.environ.get("MASTODON_ACCESS_TOKEN")
+
+mastodon_client = mastodon.Mastodon(
+    api_base_url='https://better.boston',
+    client_id=MASTODON_CLIENT_KEY,
+    client_secret=MASTODON_CLIENT_SECRET,
+    access_token=MASTODON_ACCESS_TOKEN
 )
 
 slow_zones = requests.get(
@@ -42,9 +52,13 @@ slowzones_started_yesterday = generate_new_slow_zones_list(
     slow_zones.json(), date.today()
 )
 
-tweet_text_map = generate_tweet_text_map(grouped_sz_today)
-send_tweet_threads(tweet_text_map, client)
+post_text_map = generate_post_text_map(grouped_sz_today)
 
-send_new_slow_zone_tweets(slowzones_started_yesterday, client)
+send_tweet_threads(post_text_map, twitter_client)
+send_toot_threads(post_text_map, mastodon_client)
 
-send_fixed_slow_zone_tweets(slowzones_ended_yesterday, client)
+send_new_slow_zone_tweets(slowzones_started_yesterday, twitter_client)
+send_new_slow_zone_toots(slowzones_started_yesterday, mastodon_client)
+
+send_fixed_slow_zone_tweets(slowzones_ended_yesterday, twitter_client)
+send_fixed_slow_zone_toots(slowzones_ended_yesterday, mastodon_client)
