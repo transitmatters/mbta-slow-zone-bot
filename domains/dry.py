c9f43d8e8065ffbd8fc3183e93e52a0c0e170df6
Nathan Weinberg <nathan2@stwmd.net>
2023-09-07 19:51:52 -0400

    Linting

--- domains/dry.py
+++ domains/dry.py
@@ -1,17 +1,20 @@
 from utils import format_fixed_slow_zone, format_new_slow_zone, format_updated_slow_zone
 import logging
 
+
 def send_new_slow_zone_dry(sz):
     for line in sz:
         for z in line:
             output = format_new_slow_zone(z)
             logging.info('\n' + output)
 
+
 def send_fixed_slow_zone_dry(sz):
     for line in sz:
         for z in line:
             output = format_fixed_slow_zone(z)
             logging.info('\n' + output)
+
 
 def send_updated_slow_zone_dry(sz):
     for line in sz:
