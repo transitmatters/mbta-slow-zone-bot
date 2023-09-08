a4ad117b643f3b0d20bd45a4d874ef2b73613d4c
Nathan Weinberg <nathan2@stwmd.net>
2023-09-07 20:09:48 -0400

    Added additional logging

--- domains/dry.py
+++ domains/dry.py
@@ -1,23 +1,36 @@
 from utils import format_fixed_slow_zone, format_new_slow_zone, format_updated_slow_zone
 import logging
+import sys
 
 
 def send_new_slow_zone_dry(sz):
     for line in sz:
         for z in line:
-            output = format_new_slow_zone(z)
-            logging.info("\n" + output)
+            try:
+                output = format_new_slow_zone(z)
+                logging.info("\n" + output)
+            except Exception as e:
+                logging.error(f"Error formatting output for new slow zone {z}: {e}")
+                sys.exit(1)
 
 
 def send_fixed_slow_zone_dry(sz):
     for line in sz:
         for z in line:
-            output = format_fixed_slow_zone(z)
-            logging.info("\n" + output)
+            try:
+                output = format_fixed_slow_zone(z)
+                logging.info("\n" + output)
+            except Exception as e:
+                logging.error(f"Error formatting output for fixed slow zone {z}: {e}")
+                sys.exit(1)
 
 
 def send_updated_slow_zone_dry(sz):
     for line in sz:
         for z in line:
-            output = format_updated_slow_zone(z)
-            logging.info("\n" + output)
+            try:
+                output = format_updated_slow_zone(z)
+                logging.info("\n" + output)
+            except Exception as e:
+                logging.error(f"Error formatting output for updated slow zone {z}: {e}")
+                sys.exit(1)
