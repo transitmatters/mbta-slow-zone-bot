fc9d5811c6e8f9b6646770056c95bf37b3406aa9
Seth Kaplan <sethkaplan73@gmail.com>
2025-08-27 09:29:39 -0400

    Update twitter.py

--- domains/twitter.py
+++ domains/twitter.py
@@ -23,10 +23,3 @@ def send_fixed_slow_zone_tweets(sz, client: Client):
         for z in line:
             output = format_fixed_slow_zone(z)
             client.create_tweet(text=output)
-
-
-def send_updated_slow_zone_tweets(sz, client: Client):
-    for line in sz:
-        for z in line:
-            output = format_updated_slow_zone(z)
-            client.create_tweet(text=output)
